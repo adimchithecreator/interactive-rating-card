481a3afd0673abfc3e1271ee2a5d49636b991323
Adimchi <oraegbunamadimchi@gmail.com>
2022-05-04 23:26:23 +0100

    added javascript DOM

--- app.js
+++ app.js
@@ -11,6 +11,30 @@ let btn5= document.querySelector("#five");
 function thankYouState() {
     let rateCard = sessionStorage.getItem('myRate');
     hero.innerHTML = "";
-    hero.innerHTML =
+    hero.innerHTML = `<div class="feedback">
+  <div>
+<img src="images/illustration-thank-you.svg" alt="">
+  </div>
+  <div class="select">
+ <p id="num" class="mt-3 mb-3"> You selected ${rateCard}  out of 5
+</p>
+ <h1> Thank you!</h1>
+<p>
+  We appreciate you taking the time to give a rating. If you ever need more support, 
+  don’t hesitate to get in touch! 
+</p>
+  </div>
+</div>`
+}
+
+function rateNum(e) {
+    if (e.target != submit) {
+        let rate = e.target.innerHTML;
+        console.log(rate);
+        sessionStorage.setItem('myRate', rate);
+        return rate;
+    }
     
 }
+rate.addEventListener('click', rateNum);
+submit.addEventListener('click', thankYouState);
