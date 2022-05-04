23b06c0ade42adfbb9871a01538c7b6c895d67b0
Adimchi <oraegbunamadimchi@gmail.com>
2022-05-04 23:16:38 +0100

    added thank you state and styling

---
+++ app.js
@@ -0,0 +1,16 @@
+let hero = document.querySelector('.container');
+let rate = document.querySelector('.btn');
+let submit = document.querySelector('.sub');
+let btn1 = document.querySelector('#one');
+let btn2 = document.querySelector('#two')
+let btn3 = document.querySelector("#three");
+let btn4 = document.querySelector("#four");
+let btn5= document.querySelector("#five");
+
+
+function thankYouState() {
+    let rateCard = sessionStorage.getItem('myRate');
+    hero.innerHTML = "";
+    hero.innerHTML =
+    
+}
